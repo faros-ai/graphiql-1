7cf9e3f5e0a4dd9627a96547668cff7697433476
Matthieu Monsch <matthieu.monsch@gmail.com>
2020-07-08 18:10:47 -0700

    Add prepare step
    Also remove built files.

--- resources/build.sh
+++ resources/build.sh
@@ -10,9 +10,4 @@ fi
 
 rm -rf dist/ && mkdir -p dist/
 babel src --root-mode upward --ignore __tests__ --out-dir dist/
-echo "Bundling graphiql.js..."
-browserify -g browserify-shim -s GraphiQL dist/index.js > graphiql.js
-echo "Bundling graphiql.css..."
-postcss --no-map --use autoprefixer -d dist/ css/*.css
-cat dist/*.css > graphiql.css
 echo "Done"
